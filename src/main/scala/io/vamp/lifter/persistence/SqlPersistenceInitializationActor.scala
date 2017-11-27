4ee373be3c80fd13b24868839595566dc5f60a11
mihaiSimu <mihai.simu@busymachines.com>
2017-11-27 18:20:10 +0200

    Added more controlled way of doing initialization of Persistence-initialization SQL. If the persistence actor gets called a second time, the code doesn't get re-executed

--- src/main/scala/io/vamp/lifter/persistence/SqlPersistenceInitializationActor.scala
+++ src/main/scala/io/vamp/lifter/persistence/SqlPersistenceInitializationActor.scala
@@ -1,6 +1,6 @@
 package io.vamp.lifter.persistence
 
-import akka.actor.Actor
+import akka.actor.{ Actor, ActorRef }
 import cats.data.Kleisli
 import cats.implicits.{ catsStdInstancesForList, toTraverseOps }
 import cats.instances.future.catsStdInstancesForFuture
@@ -17,66 +17,81 @@ class SqlPersistenceInitializationActor(val sqlDialectInterpreter: SqlDSL ~> Sql
 
   import PersistenceInitializationActor._
 
+  var initialized = false
+
   override def receive: Actor.Receive = {
-    case Initialize ⇒
-      val receiver = sender()
-      val url = resolveWithNamespace(Config.string("vamp.persistence.database.sql.url")())
-      val vampDatabaseUrl = Config.string("vamp.persistence.database.sql.database-server-url")()
-      val db = resolveWithNamespace(Config.string("vamp.persistence.database.sql.database")())
-      val table = Config.string("vamp.persistence.database.sql.table")()
-      val user = Config.string("vamp.persistence.database.sql.user")()
-      val password = Config.string("vamp.persistence.database.sql.password")()
-      val sqlLifterSeed = SqlLifterSeed(db, user, password, url, vampDatabaseUrl)
+    case Initialize ⇒ if (initialized) {
+      log.info(s"Init-Steps for SQL Persistence  already done for namespace ${namespace.name}; Not doing again.")
+      sender() ! Initialized
+    } else {
+      log.info(s"Performing Init-Steps for SQL Persistence ${namespace.name}")
+      doInitialization(sender());
+      log.info(s"Finished Init-Steps for SQL Persistence ${namespace.name}")
+      initialized = true
+      sender() ! Initialized
+    }
+    case _ ⇒ ()
+  }
 
-      val tableQueries: List[String] = Source
-        .fromInputStream(getClass.getResourceAsStream(sqlResource))
-        .mkString
-        .replaceAllLiterally(s"$$table", table)
-        .split(';')
-        .toList
-        .map(_.trim)
-        .filterNot(_.isEmpty)
+  def doInitialization(senderContext: ActorRef): Unit = {
+    val receiver = senderContext
+    val url = resolveWithNamespace(Config.string("vamp.persistence.database.sql.url")())
+    val vampDatabaseUrl = Config.string("vamp.persistence.database.sql.database-server-url")()
+    val db = resolveWithNamespace(Config.string("vamp.persistence.database.sql.database")())
+    val table = Config.string("vamp.persistence.database.sql.table")()
+    val user = Config.string("vamp.persistence.database.sql.user")()
+    val password = Config.string("vamp.persistence.database.sql.password")()
+    val sqlLifterSeed = SqlLifterSeed(db, user, password, url, vampDatabaseUrl)
 
-      if (sqlResource == "sqlite.sql") {
-        val createSqlLiteTableAndDatabase = for {
-          _ ← getConnection(default = true)
-          databases ← tableQueries.traverse[SqlAction, Boolean] { tableQuery ⇒
-            for {
-              connection ← getConnection(default = false)
-              statement ← createStatement(connection)
-              result ← executeStatement(statement, tableQuery)
-              _ ← closeStatement(statement)
-              _ ← closeConnection(connection)
-            } yield result
-          }.map(_.forall(identity))
-        } yield databases
+    val tableQueries: List[String] = Source
+      .fromInputStream(getClass.getResourceAsStream(sqlResource))
+      .mkString
+      .replaceAllLiterally(s"$$table", table)
+      .split(';')
+      .toList
+      .map(_.trim)
+      .filterNot(_.isEmpty)
 
-        val executeSQLiteActions: Kleisli[LifterResult, SqlLifterSeed, Boolean] =
-          createSqlLiteTableAndDatabase.foldMap(sqlDialectInterpreter)
+    if (sqlResource == "sqlite.sql") {
+      val createSqlLiteTableAndDatabase = for {
+        _ ← getConnection(default = true)
+        databases ← tableQueries.traverse[SqlAction, Boolean] { tableQuery ⇒
+          for {
+            connection ← getConnection(default = false)
+            statement ← createStatement(connection)
+            result ← executeStatement(statement, tableQuery)
+            _ ← closeStatement(statement)
+            _ ← closeConnection(connection)
+          } yield result
+        }.map(_.forall(identity))
+      } yield databases
 
-        executeSQLiteActions(sqlLifterSeed).value.foreach {
-          case Left(errorMessage) ⇒ reportException(PersistenceInitializationFailure(errorMessage))
-          case Right(_) ⇒
-            receiver ! Initialized
-            info(PersistenceInitializationSuccess)
-        }
-      } else {
-        val sqlInitCommand: LiftAction[Boolean] = for {
-          databaseCreated ← createDatabase
-          tablesCreated ← createTables(tableQueries)
-        } yield databaseCreated && tablesCreated
+      val executeSQLiteActions: Kleisli[LifterResult, SqlLifterSeed, Boolean] =
+        createSqlLiteTableAndDatabase.foldMap(sqlDialectInterpreter)
 
-        val executeSqlActions: Kleisli[LifterResult, SqlLifterSeed, Boolean] =
-          sqlInitCommand
-            .foldMap[SqlAction](sqlInterpreter)
-            .foldMap[SqlResult](sqlDialectInterpreter)
-
-        executeSqlActions(sqlLifterSeed).value.foreach {
-          case Left(errorMessage) ⇒ reportException(PersistenceInitializationFailure(errorMessage))
-          case Right(_) ⇒
-            receiver ! Initialized
-            info(PersistenceInitializationSuccess)
-        }
+      executeSQLiteActions(sqlLifterSeed).value.foreach {
+        case Left(errorMessage) ⇒ reportException(PersistenceInitializationFailure(errorMessage))
+        case Right(_) ⇒
+          receiver ! Initialized
+          info(PersistenceInitializationSuccess)
       }
+    } else {
+      val sqlInitCommand: LiftAction[Boolean] = for {
+        databaseCreated ← createDatabase
+        tablesCreated ← createTables(tableQueries)
+      } yield databaseCreated && tablesCreated
+
+      val executeSqlActions: Kleisli[LifterResult, SqlLifterSeed, Boolean] =
+        sqlInitCommand
+          .foldMap[SqlAction](sqlInterpreter)
+          .foldMap[SqlResult](sqlDialectInterpreter)
+
+      executeSqlActions(sqlLifterSeed).value.foreach {
+        case Left(errorMessage) ⇒ reportException(PersistenceInitializationFailure(errorMessage))
+        case Right(_) ⇒
+          receiver ! Initialized
+          info(PersistenceInitializationSuccess)
+      }
+    }
   }
 }
