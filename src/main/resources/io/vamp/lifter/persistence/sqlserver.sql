e9211f71fb58c39ef5f5ce4af18373195c676615
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2018-01-09 16:51:20 +0100

    either db or table namespace aware, VE-31

--- src/main/resources/io/vamp/lifter/persistence/sqlserver.sql
+++ src/main/resources/io/vamp/lifter/persistence/sqlserver.sql
@@ -1,6 +1,6 @@
 
 if not exists (select * from sysobjects where name='$table' and xtype='U')
-    create table $table (
-      ID BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-      Record VARCHAR(MAX)
+    create table [$table] (
+      [ID] BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+      [Record] VARCHAR(MAX)
     );
