e93dc8e7f08adfb53f7a5c9de1c2828855a1322a
mihaiSimu <mihai.simu@busymachines.com>
2018-01-23 17:28:26 +0200

    Bugfix for pulse initialization: When working with elasticsearch-pulse, vamp initializes a series of _template objects to make searchable indexes like vamp-pulse-<index-name>, even though the only indexes will be of form vamp-pulse-<index-name>-<date>.
    Normally this should work out of the box, however, the search fails if there are no objects to be found in those indexes, therefore, no timestamp field in any of thw objects.
    
    Added an object artificiallt as an event, to circumvent this

--- src/main/scala/io/vamp/lifter/operation/LifterBootstrap.scala
+++ src/main/scala/io/vamp/lifter/operation/LifterBootstrap.scala
@@ -1,19 +1,20 @@
 package io.vamp.lifter.operation
 
-import akka.actor.{ ActorRef, ActorSystem }
+import akka.actor.{ActorRef, ActorSystem}
 import akka.pattern.ask
 import akka.util.Timeout
-import io.vamp.common.akka.{ ActorBootstrap, IoC }
-import io.vamp.common.vitals.InfoRequest
-import io.vamp.common.{ Config, Namespace }
+import io.vamp.common.akka.{ActorBootstrap, IoC}
+import io.vamp.common.{Config, Namespace}
 import io.vamp.lifter.artifact.ArtifactInitializationActor
 import io.vamp.lifter.persistence.SqlInterpreter.SqlInterpreter
-import io.vamp.lifter.persistence.{ PersistenceInitializationActor, SqlInterpreter, SqlPersistenceInitializationActor }
+import io.vamp.lifter.persistence.{PersistenceInitializationActor, SqlInterpreter, SqlPersistenceInitializationActor}
 import io.vamp.lifter.pulse.PulseInitializationActor
 import io.vamp.model.artifact.Workflow
+import io.vamp.model.event.Event
 import io.vamp.persistence.refactor.VampPersistence
+import io.vamp.pulse.PulseActor
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
 class LifterBootstrap(implicit override val actorSystem: ActorSystem, val namespace: Namespace, override val timeout: Timeout)
     extends ActorBootstrap with VampInitialization {
@@ -99,7 +100,9 @@ trait VampInitialization {
 
   protected def setupPulse(mapping: Map[String, Any])(implicit namespace: Namespace): Future[Any] = {
     if (mapping.getOrElse("pulse", false).asInstanceOf[Boolean])
-      IoC.actorFor[PulseInitializationActor] ? PulseInitializationActor.Initialize
+      (IoC.actorFor[PulseInitializationActor] ? PulseInitializationActor.Initialize).flatMap(_ =>
+        (IoC.actorFor[PulseActor] ? PulseActor.Publish(Event(tags = Set("lifter-init"), value = "Lifter-Initialization")))
+      )
     else Future.successful(true)
   }
 
